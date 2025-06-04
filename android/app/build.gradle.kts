f2f985b1ca136a18ef709951ac18729a9a3aa397
João Sereia <joao.lo.sereia@gmail.com>
2025-06-04 16:53:27 -0300

    chore(pubspec): bump dart and flutter versions; add aurasync_ui package

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "br.com.softyes.aurasync"
     compileSdk = 36 //flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" //flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
