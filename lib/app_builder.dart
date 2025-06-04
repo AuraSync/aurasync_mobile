a658e8c55fcc0a4a04d58ec44e6dd82bf65d43bb
João Sereia <joao.lo.sereia@gmail.com>
2025-06-04 16:56:03 -0300

    feat(lib): add theme from aurasync_ui package; set the system ui mode, colors and orientations

---
+++ lib/app_builder.dart
@@ -0,0 +1,38 @@
+import 'package:aurasync_ui/aurasync_ui.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class AppBuilder extends StatelessWidget with AppThemeMixin {
+  const AppBuilder({required this.child, super.key});
+
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    _setSystemUI(context);
+    return Material(type: MaterialType.transparency, child: child);
+  }
+
+  void _setSystemUI(BuildContext context) {
+    final themeBrightness = getThemeBrightness(context);
+
+    final Brightness brightness;
+    switch (themeBrightness) {
+      case Brightness.light:
+        brightness = Brightness.dark;
+      case Brightness.dark:
+        brightness = Brightness.light;
+    }
+
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarBrightness: brightness,
+        statusBarIconBrightness: brightness,
+        systemNavigationBarIconBrightness: brightness,
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.transparent,
+        systemNavigationBarDividerColor: Colors.transparent,
+      ),
+    );
+  }
+}
