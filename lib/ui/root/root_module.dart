500cb06bef5ac112db717eaf023b3be22afb1839
João Sereia <joao.lo.sereia@gmail.com>
2025-06-07 12:30:43 -0300

    feat(ui/root): add root screen with sidebar

--- lib/ui/root/root_module.dart
+++ lib/ui/root/root_module.dart
@@ -2,6 +2,11 @@ part of 'root_screen.dart';
 
 final class RootModule extends Module {
   @override
+  void binds(Injector i) {
+    i.addSingleton<ScreenWidgetController>(ScreenWidgetController.new);
+  }
+
+  @override
   void routes(RouteManager r) {
     r.child(AppRoutes.root, child: (_) => const _RootScreen());
   }
--- lib/ui/root/root_screen.dart
+++ lib/ui/root/root_screen.dart
@@ -1,4 +1,7 @@
 import 'package:aurasync/routing/app_routes.dart';
+import 'package:aurasync/ui/_core/widgets/screen/screen_widget.dart';
+import 'package:aurasync/ui/_core/widgets/sidebar/sidebar_widget.dart';
+import 'package:aurasync_ui/aurasync_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -9,6 +12,26 @@ final class _RootScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const RouterOutlet();
+    final controller = Modular.get<ScreenWidgetController>();
+
+    return ScreenWidget(
+      controller: controller,
+      body: const RouterOutlet(),
+      sidebar: const SidebarWidget(
+        index: 0,
+        items: [
+          SidebarItem(
+            text: 'About us',
+            icon: SolarLinearIcons.infoCircle,
+            activeIcon: SolarBoldIcons.infoCircle,
+          ),
+          SidebarItem(
+            text: 'Settings',
+            icon: SolarLinearIcons.settingsMinimalistic,
+            activeIcon: SolarBoldIcons.settingsMinimalistic,
+          ),
+        ],
+      ),
+    );
   }
 }
