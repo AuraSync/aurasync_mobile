43bed4ea16715118fb8e527630f6e5da7974f4ca
João Sereia <joao.lo.sereia@gmail.com>
2025-09-25 15:13:41 -0300

    feat(lib/main): add global exception handler for command_it

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
+import 'dart:developer';
+
 import 'package:aurasync/app_builder.dart';
 import 'package:aurasync/routing/app_routes.dart';
 import 'package:aurasync/ui/root/root_screen.dart';
 import 'package:aurasync_ui/aurasync_ui.dart';
+import 'package:command_it/command_it.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  Command.reportAllExceptions = true;
+  Command.globalExceptionHandler =
+      (CommandError<dynamic> error, StackTrace stackTrace) {
+        log(error.error.toString(), stackTrace: stackTrace);
+      };
 
   await Future.wait([
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge),
